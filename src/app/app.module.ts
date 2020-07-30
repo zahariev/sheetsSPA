d0526b1f4e45f3defed9550c7419c5e4df85c111
b.zahariev <zaharievboyan@gmail.com>
2020-07-30 21:19:05 +0300

    add formModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavbarComponent
-   ],
-   imports: [
-      BrowserModule,
-      NgbModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, NgbModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
