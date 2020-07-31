5a7386117275a2b4d1579f21d1b892c200978a53
b.zahariev <zaharievboyan@gmail.com>
2020-07-31 14:53:47 +0300

    add login method

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor(pr) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    console.log(this.model);
   }
-
 }
