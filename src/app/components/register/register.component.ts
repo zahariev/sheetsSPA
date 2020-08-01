93dfc8528b169210975027d1ebb968cc6feaf9be
b.zahariev <zaharievboyan@gmail.com>
2020-08-01 08:15:03 +0300

    add login form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { AuthService } from '../../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,17 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!!localStorage.getItem('token')) this.router.navigate(['/home']);
+  }
 
   register() {
     this.authService.register(this.model).subscribe(
-      () => {
+      (user: any) => {
         console.log('registration');
+        // if (user) {
+        //   localStorage.setItem('token', user.token);
+        //   this.router.navigate(['/home']);
+        // }
       },
       (error) => {
-        console.log('not successfull');
+        console.log(error.error);
+      },
+      () => {
+        this.login();
       }
     );
   }
@@ -30,9 +40,19 @@ export class RegisterComponent implements OnInit {
         console.log('logged successfully');
       },
       (error) => {
-        console.log('logged error');
+        console.log('logged error.' + error.error);
+        this.register();
+        if (this.loggedIn) this.router.navigate(['/home']);
+      },
+      () => {
+        if (this.loggedIn) this.router.navigate(['/home']);
       }
     );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
   }
 
   cancel() {}
