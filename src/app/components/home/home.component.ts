d06fd52c0c0c9df30d2be35b4c6248ae9630ae10
b.zahariev <zaharievboyan@gmail.com>
2020-08-03 13:43:03 +0300

    add home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/_services/client.service';
+import { ProjectService } from 'src/app/_services/project.service';
+import { Client } from 'src/app/models/client';
+import { Project } from 'src/app/models/project';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  clients: Client[] = [];
+  projects: Project[] = [];
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private projectService: ProjectService
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.clients = await this.getClients();
+    this.projects = await this.getProjects();
   }
 
+  async getClients() {
+    let clients = await this.clientService.getClients().toPromise();
+
+    return clients;
+  }
+
+  async getProjects() {
+    let data = await this.projectService.getProjects().toPromise();
+
+    return data;
+  }
 }
