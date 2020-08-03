62622c4d1190bfa448069b855e8eaf5c88347f39
b.zahariev <zaharievboyan@gmail.com>
2020-08-03 13:42:42 +0300

    add httpOption authorize token

--- src/app/_services/client.service.ts
+++ src/app/_services/client.service.ts
@@ -1,8 +1,14 @@
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Client } from '../models/client';
 import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + localStorage.getItem('token'),
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +19,22 @@ export class ClientService {
   constructor(private http: HttpClient) {}
 
   getClient(id: number): Observable<Client> {
-    return this.http.get<Client>(this.baseURL + 'clients/' + id);
+    return this.http.get<Client>(this.baseURL + 'clients/' + id, httpOptions);
   }
 
   getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(this.baseURL + 'clients');
+    return this.http.get<Client[]>(this.baseURL + 'clients', httpOptions);
   }
 
   updateClient(id: number, client: Client) {
     return this.http.put(this.baseURL + 'clients/' + id, client);
+  }
+
+  deleteClient(id: number) {
+    return this.http.delete(this.baseURL + 'clients/' + id, httpOptions);
+  }
+
+  insertClient(): Observable<any> {
+    return this.http.post(this.baseURL + 'clients', {}, httpOptions);
   }
 }
